bbd29102a93c7b0ab2000bc73cd61a417755250a
csr4422 <csr935640@gmail.com>
2025-04-12 19:03:15 +0530

    Front End setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,14 +10,21 @@ import { connectDB } from "./lib/db.js";
 
 import cookieParser from "cookie-parser";
 
+import {cors} from "cors"
+
 
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
-app.use(cookieParser())
-
+app.use(cookieParser());
+app.use(cors(
+    {
+        origin:"http://localhost:5173",
+        credentials:true
+    }
+));
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 

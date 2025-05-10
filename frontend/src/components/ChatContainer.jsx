cb660e3f380362d409622dad3b08d4eda257895b
csr4422 <csr935640@gmail.com>
2025-05-10 19:49:23 +0530

    created message input space

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -23,6 +23,7 @@ const ChatContainer = () => {
    }
     
 
+   
 
   return (
     <div className='flex-1 flex flex-col overflow-auto'>
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import {create} from "zustand";
 import { axiosInstance} from "../lib/Axios";
 import toast from "react-hot-toast";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set,get) => ({
     messages:[],
     users:[],
     selectedUser:null,
@@ -31,7 +31,15 @@ export const useChatStore = create((set) => ({
         set({ isMessagesLoading: false });
       }
     },
-    
+    sendMessage: async (data) => {
+      const {selectedUser,messages} = get();
+      try {
+        const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+        set({ messages: [...messages, res.data] });
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    },
 setSelectedUser:(seletctedUser)=>{
     set({selectedUser:seletctedUser})
 }
